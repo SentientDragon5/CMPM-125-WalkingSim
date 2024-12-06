68f570c015d96c61eb35ac952956b67112aa703b
SentientDragon5 <34078184+SentientDragon5@users.noreply.github.com>
2024-12-06 15:23:27 -0800

    ui manager

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<Canvas> menus;
+    public int current_menu;
+    public List<UnityEvent> onEnterMenu;
+
+
+    void Awake()
     {
-        
+        Cursor.lockState = CursorLockMode.Confined;
+        foreach (var m in menus)
+            m.enabled = false;
+        menus[current_menu].enabled = true;
+        onEnterMenu[current_menu].Invoke();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OpenMenu(int index)
     {
-        
+        menus[current_menu].enabled = false;
+        current_menu = index;
+        menus[current_menu].enabled = true;
+        onEnterMenu[current_menu].Invoke();
     }
+
+    public void Quit() => Application.Quit();
 }
